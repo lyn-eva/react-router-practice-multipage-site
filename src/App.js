e8719fff8e84989782a535b68076e77c5dca689f
Zeiyar Lynn <zeiyarlynn@gmail.com>
2022-02-10 14:04:42 +0630

    added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
-import { Fragment } from "react";
+import { lazy, Fragment, Suspense } from "react";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 import Header from "./components/Header";
-import Home from "./components/layout/Home";
-import Destination from "./components/layout/Destination";
-import Planet from "./components/Planet";
-import Crew from "./components/layout/Crew";
-import Biography from "./components/Biography";
-import Technology from "./components/layout/Technology";
-import Procedure from "./components/Procedure";
+
+const Home = lazy(() => import("./components/layout/Home"));
+const Destination = lazy(() => import("./components/layout/Destination"));
+const Planet = lazy(() => import("./components/Planet"));
+const Crew = lazy(() => import("./components/layout/Crew"));
+const Biography = lazy(() => import("./components/Biography"));
+const Technology = lazy(() => import("./components/layout/Technology"));
+const Procedure = lazy(() => import("./components/Procedure"));
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Routes>
-        <Route path="/" element={<Navigate to="home" />} />
-        <Route path="home" element={<Home />} />
-        <Route path="destination" element={<Destination />}>
-          <Route path=":id/:planet" element={<Planet />} />
-        </Route>
-        <Route path="crew" element={<Crew />}>
-          <Route path=":id" element={<Biography />} />
-        </Route>
-        <Route path="technology" element={<Technology />}>
-          <Route path=":id" element={<Procedure />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<p className='fixed left-1/2 top-1/2 -translate-x-1/2 text-2xl'>Loading ...</p>}>
+        <Routes>
+          <Route path="/" element={<Navigate to="home" />} />
+          <Route path="home" element={<Home />} />
+          <Route path="destination" element={<Destination />}>
+            <Route path=":id/:planet" element={<Planet />} />
+          </Route>
+          <Route path="crew" element={<Crew />}>
+            <Route path=":id" element={<Biography />} />
+          </Route>
+          <Route path="technology" element={<Technology />}>
+            <Route path=":id" element={<Procedure />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
